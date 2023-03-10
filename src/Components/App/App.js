89708cb9603e21f07642f3396f84fcd46ffd96de
Tim Poberezhnyi <poberezhny003@gmail.com>
2023-03-10 18:28:03 +0100

    Adding SearchBar and Button

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import SearchBar from '../SearchBar/SearchBar';
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* <SearchBar /> */}
+          <SearchBar />
           <div className="App-playlist">
             {/* <SearchResults /> */}
             {/* <Playlist /> */}
